11a0992cec71884513de0f1d0f978e185b4f36e5
John Spurlock <john@skymethod.com>
2023-11-12 16:32:09 -0600

    Ability to specify custom v8 encoder/decoder to npm openKv.

--- src/npm.ts
+++ src/npm.ts
@@ -9,17 +9,31 @@ export { UnknownV8 } from './v8.ts';
 
 export async function openKv(path?: string, opts: Record<string, unknown> & { debug?: boolean } = {}) {
     const debug = opts.debug === true;
+
     if (path === undefined || path === '') return await makeInMemoryService({ debug }).openKv(path);
     
-    const v8 = await import(`${'v8'}`);
-    if (!v8) throw new Error(`Unable to import the v8 module`);
-    const encodeV8: EncodeV8 = v8.serialize;
-    const decodeV8: DecodeV8 = v8.deserialize;
+    const { encodeV8, decodeV8 } = await (async () => {
+        const { encodeV8, decodeV8 } = opts;
+        const defined = [ encodeV8, decodeV8 ].filter(v => v !== undefined).length;
+        if (defined === 1) throw new Error(`Provide both 'encodeV8' or 'decodeV8', or neither`);
+        if (defined > 0) {
+            if (typeof encodeV8 !== 'function') throw new Error(`Unexpected 'encodeV8': ${encodeV8}`);
+            if (typeof decodeV8 !== 'function') throw new Error(`Unexpected 'decodeV8': ${decodeV8}`);
+            return { encodeV8: encodeV8 as EncodeV8, decodeV8: decodeV8 as DecodeV8 };
+        }
+        const v8 = await import(`${'v8'}`);
+        if (!v8) throw new Error(`Unable to import the v8 module`);
+        const { serialize, deserialize } = v8;
+        if (typeof serialize !== 'function') throw new Error(`Unexpected 'serialize': ${serialize}`);
+        if (typeof deserialize !== 'function') throw new Error(`Unexpected 'deserialize': ${deserialize}`);
+        return { encodeV8: serialize as EncodeV8, decodeV8: deserialize as DecodeV8 };
+    })();
+
     if (/^https?:\/\//i.test(path)) {
         // deno-lint-ignore no-explicit-any
         const accessToken = (globalThis as any)?.process?.env?.DENO_KV_ACCESS_TOKEN;
         if (typeof accessToken !== 'string') throw new Error(`Set the DENO_KV_ACCESS_TOKEN to your access token`);
-        return await makeRemoteService({ debug, accessToken }).openKv(path);
+        return await makeRemoteService({ debug, accessToken, encodeV8, decodeV8 }).openKv(path);
     }
     const { napi } = opts;
     if (!isNapiInterface(napi)) throw new Error(`Provide the napi interface for sqlite`);
