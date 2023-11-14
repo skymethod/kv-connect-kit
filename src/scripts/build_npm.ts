ae41f63dc58654acb69298ba3014b8fe9750c0f1
John Spurlock <john@skymethod.com>
2023-11-14 16:07:03 -0600

    Utilize new dnt for disposable types, remove filter workaround.

--- src/scripts/build_npm.ts
+++ src/scripts/build_npm.ts
@@ -1,5 +1,5 @@
 import { join } from 'https://deno.land/std@0.206.0/path/mod.ts';
-import { LibName, build, emptyDir } from 'https://deno.land/x/dnt@0.38.1/mod.ts';
+import { LibName, build, emptyDir } from 'https://deno.land/x/dnt@0.39.0/mod.ts';
 import { parse as parseFlags } from 'https://deno.land/std@0.206.0/flags/mod.ts';
 
 const flags = parseFlags(Deno.args);
@@ -18,17 +18,8 @@ await build({
     },
     compilerOptions: {
         // let's try to support Node 18+
-        lib: [ 'ES2020', 'DOM', 'DOM.Iterable', ...(tests ? [ 'ES2021.WeakRef' as LibName ] : []) ],
+        lib: [ 'ES2020', 'DOM', 'DOM.Iterable', 'ESNext.Disposable', ...(tests ? [ 'ES2021.WeakRef' as LibName ] : []) ],
         target: 'ES2020',
-    },
-    filterDiagnostic: v => {
-        const { code, category, file, messageText } = v;
-        const txt = typeof messageText === 'string' ? messageText : messageText.messageText;
-        // Symbole.dispose is too new for dnt type-checking: https://github.com/denoland/dnt/issues/345
-        if (code === 1169 && file?.fileName.endsWith('/kv_types.ts') && txt.includes('unique symbol')) return false;
-        if (code === 2339 && /\/kv_(types|util)\.ts$/.test(file?.fileName ?? '') && txt.includes(`Property 'dispose' does not exist on type 'SymbolConstructor'`)) return false;
-        console.log(JSON.stringify({ code, category, file: file?.fileName, messageText }, undefined, 2));
-        return true;
     },
     package: {
         // package.json properties
