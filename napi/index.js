cffea6efc54d5a551824255ac17e6e409cb9aa84
John Spurlock <john@skymethod.com>
2023-11-14 18:38:50 -0600

    napi: Fix build for scopes.

--- napi/index.js
+++ napi/index.js
@@ -37,7 +37,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./kv-connect-kit-napi.android-arm64.node')
           } else {
-            nativeBinding = require('kv-connect-kit-napi-android-arm64')
+            nativeBinding = require('@skymethod/kv-connect-kit-napi-android-arm64')
           }
         } catch (e) {
           loadError = e
@@ -49,7 +49,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./kv-connect-kit-napi.android-arm-eabi.node')
           } else {
-            nativeBinding = require('kv-connect-kit-napi-android-arm-eabi')
+            nativeBinding = require('@skymethod/kv-connect-kit-napi-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -67,7 +67,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./kv-connect-kit-napi.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('kv-connect-kit-napi-win32-x64-msvc')
+            nativeBinding = require('@skymethod/kv-connect-kit-napi-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -79,7 +79,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./kv-connect-kit-napi.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('kv-connect-kit-napi-win32-ia32-msvc')
+            nativeBinding = require('@skymethod/kv-connect-kit-napi-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -91,7 +91,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./kv-connect-kit-napi.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('kv-connect-kit-napi-win32-arm64-msvc')
+            nativeBinding = require('@skymethod/kv-connect-kit-napi-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -107,7 +107,7 @@ switch (platform) {
       if (localFileExisted) {
         nativeBinding = require('./kv-connect-kit-napi.darwin-universal.node')
       } else {
-        nativeBinding = require('kv-connect-kit-napi-darwin-universal')
+        nativeBinding = require('@skymethod/kv-connect-kit-napi-darwin-universal')
       }
       break
     } catch {}
@@ -118,7 +118,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./kv-connect-kit-napi.darwin-x64.node')
           } else {
-            nativeBinding = require('kv-connect-kit-napi-darwin-x64')
+            nativeBinding = require('@skymethod/kv-connect-kit-napi-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -130,7 +130,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./kv-connect-kit-napi.darwin-arm64.node')
           } else {
-            nativeBinding = require('kv-connect-kit-napi-darwin-arm64')
+            nativeBinding = require('@skymethod/kv-connect-kit-napi-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -149,7 +149,7 @@ switch (platform) {
       if (localFileExisted) {
         nativeBinding = require('./kv-connect-kit-napi.freebsd-x64.node')
       } else {
-        nativeBinding = require('kv-connect-kit-napi-freebsd-x64')
+        nativeBinding = require('@skymethod/kv-connect-kit-napi-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -164,7 +164,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./kv-connect-kit-napi.linux-x64-musl.node')
             } else {
-              nativeBinding = require('kv-connect-kit-napi-linux-x64-musl')
+              nativeBinding = require('@skymethod/kv-connect-kit-napi-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
@@ -175,7 +175,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./kv-connect-kit-napi.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('kv-connect-kit-napi-linux-x64-gnu')
+              nativeBinding = require('@skymethod/kv-connect-kit-napi-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -189,7 +189,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./kv-connect-kit-napi.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('kv-connect-kit-napi-linux-arm64-musl')
+              nativeBinding = require('@skymethod/kv-connect-kit-napi-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
@@ -200,7 +200,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./kv-connect-kit-napi.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('kv-connect-kit-napi-linux-arm64-gnu')
+              nativeBinding = require('@skymethod/kv-connect-kit-napi-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -213,7 +213,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./kv-connect-kit-napi.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('kv-connect-kit-napi-linux-arm-gnueabihf')
+            nativeBinding = require('@skymethod/kv-connect-kit-napi-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
